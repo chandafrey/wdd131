07d29e1f44794af58e078e87fe82fd000eae2fa1
chandafrey <chanda.frey09@gmail.com>
2025-03-21 23:34:04 -0600

    started adding place-large.css information

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -1,26 +1,31 @@
-// WINCHILL Function--------------------------------------
-// To get the Windchill
+// WINCHILL Function------
+
+// In html you need something like this line:
+/* <p id="windchillResult">Calculating Windchill...</p> */
+
 // const temp = parseFloat(document.getElementById("temp").textContent);
 // const speed = parseFloat(document.getElementById("speed").textContent);
-const temp = 45;
-const speed = 5;    
+const temperature = 45;
+const windspeed = 5;    
 
-function calculateWindchill(temp, speed) {
+function calculateWindChill(temperature, windspeed) {
     // const windchill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-    const windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+    const windchill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16));
     return windchill;
 }  
 
-const windchill = calculateWindchill(temp, speed);
+// Calculate windchill using static values
+const windchill = calculateWindChill(temperature, windspeed);
 
+// Check if windchill conditions are met..<=50 degrees and > 3 mph
 let result;
-if (temp <= 50 && speed > 3) {
-    result = Math.round(windchill);
+if (temperature <= 50 && windspeed > 3) {
+    result = Math.round(windchill); //Round result if conditions are met
 } else {
-    result = "N/A";
+    result = "N/A"; //Display N/A if conditions are not met
 }
 
-
+// Display results on page...different ways to write this
 // document.getElementById("windchillResult").textContent = ' ${windchill}°F';
 // document.getElementById('windchillResult').innerText = ` ${windchill.toFixed(2)}°F`;
 document.getElementById('windchillResult').innerText = `${result}°F`;
