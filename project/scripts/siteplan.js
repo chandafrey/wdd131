363447bc8a958681c49a3c8eadc0a85b0d923005
chandafrey <chanda.frey09@gmail.com>
2025-04-05 00:46:59 -0600

    added wireframe images and worked on layout

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -9,3 +9,14 @@ hamButton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     hamButton.classList.toggle("open");
 });
+
+
+//FOOTER DATES-------------------------------------- 
+// To get "lastModified", you need these two lines
+let lastModified = document.lastModified;
+// ${lastModified} and the backticks`` make this line a template literal and more simple to read
+document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
+
+// To get "currentYear"
+let currentYear = new Date().getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
